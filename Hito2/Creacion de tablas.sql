e417aecbf9d3f933bb490dd389992aa04c699c43
ErYavi <96310629+ErYavi@users.noreply.github.com>
2022-11-20 18:00:13 +0100

    Update Creacion de tablas.sql

--- Hito2/Creacion de tablas.sql
+++ Hito2/Creacion de tablas.sql
@@ -10,13 +10,13 @@ CREATE TABLE Cartero
 (
   DNI_Car VARCHAR(9) NOT NULL,
   Nom_C VARCHAR(20) NOT NULL,
-  Apellidos_C VARCHAR(20) NOT NULL,
+  Apellidos_C VARCHAR(30) NOT NULL,
   PRIMARY KEY (DNI_Car)
 );
 
 CREATE TABLE Ruta
 (
-  ID_Ruta INT NOT NULL,
+  ID_Ruta VARCHAR(9) NOT NULL,
   PRIMARY KEY (ID_Ruta)
 );
 
@@ -63,7 +63,7 @@ CREATE TABLE Direccion
 
 CREATE TABLE Centro_C
 (
-  Cod_CC INT NOT NULL,
+  Cod_CC VARCHAR(9) NOT NULL,
   NombreCC VARCHAR(20) NOT NULL,
   Max_pq INT NOT NULL,
   Max_ct INT NOT NULL,
@@ -76,7 +76,7 @@ CREATE TABLE Segmento
 (
   N_ini INT NOT NULL,
   N_fin INT NOT NULL,
-  ID_Seg INT NOT NULL,
+  ID_Seg VARCHAR(9) NOT NULL,
   NCalle VARCHAR(40) NOT NULL,
   PRIMARY KEY (ID_Seg),
   FOREIGN KEY (NCalle) REFERENCES Calle(NCalle)
@@ -86,7 +86,7 @@ CREATE TABLE Usuario_Gen
 (
   ID_Gen INT NOT NULL,
   Nom_Gen VARCHAR(20) NOT NULL,
-  Apellidos_Gen VARCHAR(20) NOT NULL,
+  Apellidos_Gen VARCHAR(30) NOT NULL,
   Numero INT NOT NULL,
   Piso INT NOT NULL,
   Letra INT NOT NULL,
@@ -98,8 +98,8 @@ CREATE TABLE Usuario_Gen
 CREATE TABLE Ruta_Incuye_Seg
 (
   NOrden INT NOT NULL,
-  ID_Seg INT NOT NULL,
-  ID_Ruta INT NOT NULL,
+  ID_Seg VARCHAR(9) NOT NULL,
+  ID_Ruta VARCHAR(9) NOT NULL,
   PRIMARY KEY (ID_Seg, ID_Ruta),
   FOREIGN KEY (ID_Seg) REFERENCES Segmento(ID_Seg),
   FOREIGN KEY (ID_Ruta) REFERENCES Ruta(ID_Ruta),
@@ -108,9 +108,9 @@ CREATE TABLE Ruta_Incuye_Seg
 
 CREATE TABLE Oficina
 (
-  Cod_O INT NOT NULL,
+  Cod_O VARCHAR(9) NOT NULL,
   NMunicipio VARCHAR(40) NOT NULL,
-  Cod_CC INT NOT NULL,
+  Cod_CC VARCHAR(9) NOT NULL,
   PRIMARY KEY (Cod_O),
   FOREIGN KEY (NMunicipio) REFERENCES Municipio(NMunicipio),
   FOREIGN KEY (Cod_CC) REFERENCES Centro_C(Cod_CC)
@@ -120,7 +120,7 @@ CREATE TABLE Coche
 (
   Capacidad INT NOT NULL,
   Matricula VARCHAR(7) NOT NULL,
-  Cod_O INT NOT NULL,
+  Cod_O VARCHAR(9) NOT NULL,
   PRIMARY KEY (Matricula),
   FOREIGN KEY (Cod_O) REFERENCES Oficina(Cod_O)
 );
@@ -128,9 +128,9 @@ CREATE TABLE Coche
 CREATE TABLE Reparto
 (
   FechaR DATE NOT NULL,
-  ID_Reparto INT NOT NULL,
+  ID_Reparto VARCHAR(9) NOT NULL,
   DNI VARCHAR(9) NOT NULL,
-  ID_Ruta INT NOT NULL,
+  ID_Ruta VARCHAR(9) NOT NULL,
   Matricula VARCHAR(7) NOT NULL,
   PRIMARY KEY (ID_Reparto),
   FOREIGN KEY (DNI) REFERENCES Cartero(DNI_Car),
@@ -140,8 +140,8 @@ CREATE TABLE Reparto
 
 CREATE TABLE Area_envio
 (
-  ID_Area INT NOT NULL,
-  Cod_O INT NOT NULL,
+  ID_Area VARCHAR(9) NOT NULL,
+  Cod_O VARCHAR(9) NOT NULL,
   PRIMARY KEY (ID_Area),
   FOREIGN KEY (Cod_O) REFERENCES Oficina(Cod_O)
 );
@@ -150,7 +150,7 @@ CREATE TABLE Trabajo
 (
   Fecha_ini DATE NOT NULL,
   Jornada VARCHAR(30) NOT NULL,
-  Cod_O INT NOT NULL,
+  Cod_O VARCHAR(9) NOT NULL,
   DNI_Car VARCHAR(9) NOT NULL,
   PRIMARY KEY (Jornada, Cod_O, DNI_Car),
   FOREIGN KEY (Jornada) REFERENCES Turno(Jornada),
@@ -160,8 +160,8 @@ CREATE TABLE Trabajo
 
 CREATE TABLE Area_Incluye_Seg
 (
-  ID_Area INT NOT NULL,
-  ID_Seg INT NOT NULL,
+  ID_Area VARCHAR(9) NOT NULL,
+  ID_Seg VARCHAR(9) NOT NULL,
   PRIMARY KEY (ID_Area, ID_Seg),
   FOREIGN KEY (ID_Area) REFERENCES Area_envio(ID_Area),
   FOREIGN KEY (ID_Seg) REFERENCES Segmento(ID_Seg)
@@ -169,8 +169,8 @@ CREATE TABLE Area_Incluye_Seg
 
 CREATE TABLE Area_Incluye_Area
 (
-  ID_Padre INT NOT NULL,
-  ID_Hijo INT NOT NULL,
+  ID_Padre VARCHAR(9) NOT NULL,
+  ID_Hijo VARCHAR(9) NOT NULL,
   PRIMARY KEY (ID_Padre, ID_Hijo),
   FOREIGN KEY (ID_Padre) REFERENCES Area_envio(ID_Area),
   FOREIGN KEY (ID_Hijo) REFERENCES Area_envio(ID_Area)
@@ -178,7 +178,7 @@ CREATE TABLE Area_Incluye_Area
 
 CREATE TABLE Cartero_Reparte_Area
 (
-  ID_Area INT NOT NULL,
+  ID_Area VARCHAR(9) NOT NULL,
   DNI_Car VARCHAR(9) NOT NULL,
   PRIMARY KEY (ID_Area, DNI_Car),
   FOREIGN KEY (ID_Area) REFERENCES Area_envio(ID_Area),
@@ -188,11 +188,11 @@ CREATE TABLE Cartero_Reparte_Area
 CREATE TABLE Carta
 (
   CT VARCHAR (12) NOT NULL,
-  Formato INT NOT NULL,
+  Formato VARCHAR(2) NOT NULL,
   DNI_Car VARCHAR(9) NOT NULL,
   ID_Emisor INT NOT NULL,
   ID_Receptor INT NOT NULL,
-  ID_Reparto INT NOT NULL,
+  ID_Reparto VARCHAR(9) NOT NULL,
   PRIMARY KEY (CT),
   FOREIGN KEY (DNI_Car) REFERENCES Cartero(DNI_Car),
   FOREIGN KEY (ID_Emisor) REFERENCES Usuario_Gen(ID_Gen),
@@ -213,7 +213,7 @@ CREATE TABLE Usuario_Iden
 
 CREATE TABLE Recogida
 (
-  ID_Rec INT NOT NULL,
+  ID_Rec VARCHAR(9) NOT NULL,
   Fecha_Rec DATE NOT NULL,
   Numero INT NOT NULL,
   Piso INT NOT NULL,
@@ -232,11 +232,11 @@ CREATE TABLE Paquete
   PQ VARCHAR (12) NOT NULL,
   Dimensiones VARCHAR(20) NOT NULL,
   Peso FLOAT NOT NULL,
-  ID_Rec INT NOT NULL,
+  ID_Rec VARCHAR(9),
   ID_Emisor INT NOT NULL,
   ID_Receptor INT NOT NULL,
   DNI_Car VARCHAR(9) NOT NULL,
-  ID_Reparto INT NOT NULL,
+  ID_Reparto VARCHAR(9) NOT NULL,
   PRIMARY KEY (PQ),
   FOREIGN KEY (ID_Rec) REFERENCES Recogida(ID_Rec),
   FOREIGN KEY (ID_Emisor) REFERENCES Usuario_Gen(ID_Gen),
@@ -248,11 +248,11 @@ CREATE TABLE Paquete
 CREATE TABLE Certificada
 (
   CE VARCHAR (12) NOT NULL,
-  Urgencia INT NOT NULL,
+  Urgencia VARCHAR(10) NOT NULL,
   DNI_Emisor VARCHAR(9) NOT NULL,
   DNI_Receptor VARCHAR(9) NOT NULL,
   DNI_Car VARCHAR(9) NOT NULL,
-  ID_Reparto INT NOT NULL,
+  ID_Reparto VARCHAR(9) NOT NULL,
   PRIMARY KEY (CE),
   FOREIGN KEY (DNI_Emisor) REFERENCES Usuario_Iden(DNI_Usuario),
   FOREIGN KEY (DNI_Receptor) REFERENCES Usuario_Iden(DNI_Usuario),
