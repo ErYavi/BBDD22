4c728b88f5c6de60295eac3e9ea15cfd30240d8d
ErYavi <96310629+ErYavi@users.noreply.github.com>
2022-11-20 18:31:41 +0100

    Create Inserts_Correos.sql

---
+++ Hito2/Inserts_Correos.sql
@@ -0,0 +1,8 @@
+INSERT INTO Cartero (DNI_Car, Nom_C, Apellidos_C) VALUES
+('01542462N','Carlos','Castillo Romero');
+
+INSERT INTO Ruta (ID_Ruta) VALUES
+('RU-MAD-0001');
+
+INSERT INTO Turno (Jornada, Horario) VALUES
+('Mañana', '09:00-13:00')
