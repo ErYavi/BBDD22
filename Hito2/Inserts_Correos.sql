44cfbc089b0a0271621c95173ddc426c8f4fa2b3
AndreiDanielC <118894676+AndreiDanielC@users.noreply.github.com>
2022-11-24 18:56:35 +0100

    Update Inserts_Correos.sql

--- Hito2/Inserts_Correos.sql
+++ Hito2/Inserts_Correos.sql
@@ -1,11 +1,20 @@
 INSERT INTO Cartero (DNI_Car, Nom_C, Apellidos_C) VALUES
-('01542462N','Carlos','Castillo Romero');
+('01542462N','Carlos','Castillo Romero'),
+('54156156I','Paco','Montes'),
+('15621566H','Nicolae','Neagu'),
+('98854312B','Francisco','Fernandez Burgos');
 
 INSERT INTO Ruta (ID_Ruta) VALUES
-('RU-MAD-0001');
+('RU-MAD-0001'),
+('RU-MAD-0002'),
+('RU-MAD-0003'),
+('RU-MAD-0004'),
+('RU-AVI-0001'),
+('RU-CAD-0001');
 
 INSERT INTO Turno (Jornada, Horario) VALUES
-('Mañana', '09:00-13:00');
+('Mañana', '09:00-13:00'),
+('Tarde', '15:00-19:00');
 
 INSERT INTO Provincia (NProvincia) VALUES
 ('Cadiz'),
@@ -16,7 +25,8 @@ INSERT INTO Municipio (NMunicipio, NProvincia) VALUES
 ('Jerez de la Frontera', 'Cadiz'),
 ('Avila', 'Avila'),
 ('Madrid', 'Madrid'),
-('Alcobendas', 'Madrid');
+('Alcobendas', 'Madrid'),
+('Alcorcon','Madrid');
 
 INSERT INTO Calle (Ncalle,Nmunicipio) VALUES
 ('Paseo de la Chopera','Alcobendas'),
@@ -25,26 +35,109 @@ INSERT INTO Calle (Ncalle,Nmunicipio) VALUES
 ('Paseo de la Castellana', 'Madrid'),
 ('Calle Mayor', 'Alcobendas'),
 ('Calle Mayor', 'Madrid'),
-('Paseo de San Roque', 'Avila');
+('Paseo de San Roque', 'Alcorcon'),
+('Avenida de America','Alcorcon'),
+('Avenida de America','Madrid');
 
 INSERT INTO Direccion (Numero, Piso, Letra, Portal, NCalle, NMunicipio) VALUES # NA significa que NO APLICA.
 ('3','1','D','F','Paseo de Europa', 'Alcobendas'),
 ('39','4', 'NA', 'NA','Paseo de la Chopera', 'Alcobendas'),
-('23','NA','NA','NA','Paseo de San Roque', 'Avila'),
+('23','NA','NA','NA','Paseo de San Roque', 'Alcorcon'),
 ('49','4', 'NA', 'NA','Paseo de la Castellana', 'Madrid'),
-('9','2', 'B', 'NA','Calle Mayor', 'Madrid');
+('9','2', 'B', 'NA','Calle Mayor', 'Madrid'),
+('22','6','H','C','Avenida de America','Alcorcon'),
+('6','22','NA','NA','Avenida de America','Madrid');
 
 INSERT INTO Centro_C (Cod_CC, NombreCC, Max_pq, Max_ct, NMunicipio) VALUES
 ('CC-MAD-01','Madrid Centro',550,1750,'Madrid'),
 ('CC-MAD-02','Madrid Norte',5000,80000,'Madrid'),
 ('CC-MAD-03','Madrid Sur',2000,10000,'Madrid'),
-('CC-AV-01','Avila 1',550,1750,'Avila'),
+('CC-MAD-11','Alcorcon 1',550,1750,'Alcorcon'),
 ('CC-CA-01','Madrid Centro 1',550,1750,'Jerez de la Frontera'),
 ('CC-MAD-04','Alcobendas',8000,100000,'Alcobendas');
 
 INSERT INTO Segmento (N_ini, N_fin, ID_Seg, NCalle, NMunicipio) VALUES
 (1,50,'SE-MAD-01','Paseo de la Castellana', 'Madrid'),
-(50,100,'SE-MAD-02','Paseo de la Castellana', 'Madrid'),
-(1,20,'SE-AV-03','Paseo de San Roque', 'Avila'),
-(1,50,'SE-ALC-04','Paseo de la Chopera', 'Alcobendas'),
-(1,50,'SE-ALC-05','Paseo de la Chopera', 'Alcobendas');
+(51,100,'SE-MAD-02','Paseo de la Castellana', 'Madrid'),
+(1,20,'SE-ALC-04','Paseo de San Roque', 'Alcorcon'),
+(1,50,'SE-ACB-04','Paseo de la Chopera', 'Alcobendas'),
+(1,50,'SE-ACB-05','Paseo de la Chopera', 'Alcobendas'),
+(1,20,'SE-ALC-11','Avenida de America', 'Alcorcon'),
+(21,50,'SE-ALC-12','Avenida de America', 'Alcorcon');
+
+INSERT INTO Usuario_Gen(Id_Gen,Nom_Gen,Apellidos_Gen,Numero,Piso,Letra,Portal,Ncalle,Nmunicipio) VALUES
+(1,'Juan','Illo','3','1','D','F','Paseo de Europa', 'Alcobendas'),
+(2,'Hermegildo','Rupert','39','4', 'NA', 'NA','Paseo de la Chopera', 'Alcobendas'),
+(3,'Jhonny','Test','23','NA','NA','NA','Paseo de San Roque', 'Alcorcon'),
+(4,'Ruben','Doblas','49','4', 'NA', 'NA','Paseo de la Castellana', 'Madrid'),
+(5,'Martin','Mendez','9','2', 'B', 'NA','Calle Mayor', 'Madrid'),
+(6,'Pedro','Perez','22','6','H','C','Avenida de America','Alcorcon'),
+(7,'Mohammed','Al-Ahbuki','6','22','NA','NA','Avenida de America','Madrid');
+
+INSERT INTO Ruta_Incuye_Seg(Norden,ID_seg,ID_ruta,Ncalle,Nmunicipio) VALUES
+('1','SE-MAD-01','RU-MAD-0001','Paseo de la Castellana', 'Madrid'),
+('2','SE-MAD-02','RU-MAD-0001','Paseo de la Castellana', 'Madrid'),
+('1','SE-ALC-11','RU-MAD-0002','Avenida de America', 'Alcorcon'),
+('2','SE-ALC-04','RU-MAD-0002','Paseo de San Roque', 'Alcorcon'),
+('1','SE-ALC-11','RU-MAD-0003','Avenida de America', 'Alcorcon'),
+('2','SE-ALC-12','RU-MAD-0003','Avenida de America', 'Alcorcon');
+
+INSERT INTO Oficina(Cod_O,Nmunicipio,Cod_CC) VALUES
+('OF-ALC-01','Alcorcon','CC-MAD-11'),
+('OF-MAD-01','Madrid','CC-MAD-01'),
+('OF-MAD-02','Madrid','CC-MAD-02'),
+('OF-MAD-03','Madrid','CC-MAD-03'),
+('OF-ACB-01','Alcobendas','CC-MAD-04'),
+('OF-ACB-02','Alcobendas','CC-MAD-04');
+
+INSERT INTO Coche(Capacidad,Matricula,Cod_O) VALUES
+(0.75,'4563CBH','OF-ALC-01'),
+(1,'6598JKY','OF-ACB-01'),
+(1.5,'1285DLK','OF-MAD-02'),
+(1.25,'9421MFD','OF-MAD-03');
+
+INSERT INTO Reparto(FechaR,Id_Reparto,DNI_Car,Id_ruta,Matricula) VALUES
+('2022-06-23','RE-MAD-01','98854312B','RU-MAD-0001','4563CBH'),
+('2022-09-15','RE-MAD-02','01542462N','RU-MAD-0002','6598JKY'),
+('2022-01-03','RE-MAD-03','54156156I','RU-MAD-0002','6598JKY'),
+('2022-08-05','RE-ACB-01','15621566H','RU-MAD-0001','1285DLK'),
+('2022-10-01','RE-ACB-02','01542462N','RU-MAD-0003','9421MFD'),
+('2022-07-21','RE-ALC-01','98854312B','RU-MAD-0003','9421MFD');
+
+INSERT INTO Area_envio(ID_Area,Cod_O) VALUES
+('AR-MAD-01','OF-MAD-01'),
+('AR-MAD-02','OF-MAD-02'),
+('AR-MAD-03','OF-MAD-03'),
+('AR-ALC-01','OF-ALC-01'),
+('AR-ACB-01','OF-ACB-01'),
+('AR-ACB-02','OF-ACB-02');
+
+INSERT INTO Trabajo(Fecha_ini,Jornada,Cod_O,DNI_Car) VALUES
+('2022-01-01','Mañana','OF-MAD-01','98854312B'),
+('2022-01-01','Mañana','OF-MAD-02','01542462N'),
+('2022-01-01','Tarde','OF-MAD-03','01542462N'),
+('2022-01-01','Tarde','OF-ACB-01','54156156I'),
+('2022-01-01','Tarde','OF-ALC-01','15621566H');
+
+INSERT INTO Area_Incluye_Seg(ID_area,Id_seg) VALUES
+('AR-MAD-01','SE-MAD-01'),
+('AR-MAD-01','SE-MAD-02'),
+('AR-MAD-03','SE-ALC-11'),
+('AR-MAD-03','SE-ALC-12'),
+('AR-ALC-01','SE-ALC-04'),
+('AR-ACB-02','SE-ACB-04'),
+('AR-ACB-01','SE-ACB-05');
+
+INSERT INTO Area_Incluye_Area(Id_Padre,ID_hijo) VALUES
+('AR-MAD-01','AR-MAD-02');
+
+INSERT INTO Cartero_Reparte_Area(ID_area,DNI_Car) VALUES
+('AR-MAD-01','01542462N'),
+('AR-ALC-01','54156156I'),
+('AR-ACB-01','15621566H');
+
+INSERT INTO Carta(CT,Formato,DNI_Car,ID_Emisor,Id_Receptor,ID_reparto) VALUES
+('CT2659480245','A5','01542462N',1,4,'RE-MAD-01'),
+('CT0141252132','A4','98854312B',2,7,'RE-MAD-02'),
+('CT4445355515','A4','54156156I',3,5,'RE-ACB-01'),
+('CT7484549645','A5','15621566H',4,1,'RE-ALC-01');
